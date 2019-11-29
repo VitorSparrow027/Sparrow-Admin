be566c5b22a24f23015581902b6b676e8b80a195
VitorSparrow027 <55920025+VitorSparrow027@users.noreply.github.com>
2019-11-28 21:27:06 -0300

    51515

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+    
+    print_r(realpath(dirname(__FILE__,2).'/src/config/database.php'));
+    require_once realpath(dirname(__FILE__,2).'/src/config/database.php');
+    
+
+
+    Database::getConection();
+    
+?>
