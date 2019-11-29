26164187f67ac014fbfd60596208929e3a81d1f5
VitorSparrow027 <55920025+VitorSparrow027@users.noreply.github.com>
2019-11-28 21:49:54 -0300

    5151151

--- src/index.php
+++ src/index.php
@@ -3,6 +3,10 @@
     print_r(realpath(dirname(__FILE__,2).'/src/config/database.php'));
     require_once realpath(dirname(__FILE__,2).'/src/config/database.php');
     
+    $conexao = new Database();
+    var_dump($conexao->getConection());
+    
     Database::getConection();
+
     
 ?>
