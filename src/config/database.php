befec52198ef74117c15dca36dd09183c81a642b
VitorSparrow027 <55920025+VitorSparrow027@users.noreply.github.com>
2019-11-28 21:50:16 -0300

    56155

--- src/config/database.php
+++ src/config/database.php
@@ -1,20 +1,19 @@
 <?php
-    print_r(realpath(dirname(__FILE__,2).'/env_dev.ini'));
-    print_r(parse_ini_file(realpath(dirname(__FILE__,2).'/env_dev.ini')));
-    class Database{
+class Database{
         public static function getConection(){
             //primeiro, pego o caminho  do arquivo de configuração ENV
             $pathEnv = realpath(dirname(__FILE__,2).'/env_dev.ini');
+
             //segundo, pegar as chaves do arquivo de configuração
             $env = parse_ini_file($pathEnv);
 
-            $conn = new mysqli ($env['host'], $env ['username'],$env['password'],$env['database']);
-            if($conn->connect_error);{
-            die("Error: ". $conn->connect_error);
-        }
+            $conn = new mysqli ($env['host'],$env['username'],$env['password'],$env['database']);
+
+                if($conn->connect_error){
+                die("Error: ". $conn->connect_error);
+           }
         return $conn;
-           
-        }
+            }
 
     }
 ?>
