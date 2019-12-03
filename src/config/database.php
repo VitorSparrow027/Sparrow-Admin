ccdd76440559cdd0eeb9d98137ee6aa9a1a949b2
VitorSparrow027 <55920025+VitorSparrow027@users.noreply.github.com>
2019-12-02 21:49:53 -0300

    6515151

--- src/config/database.php
+++ src/config/database.php
@@ -1,5 +1,9 @@
 <?php
 class Database{
+
+        public function _contruct(){
+            getConection();
+        }
         public static function getConection(){
             //primeiro, pego o caminho  do arquivo de configuração ENV
             $pathEnv = realpath(dirname(__FILE__,2).'/env_dev.ini');
@@ -8,6 +12,8 @@ class Database{
             $env = parse_ini_file($pathEnv);
 
             $conn = new mysqli ($env['host'],$env['username'],$env['password'],$env['database']);
+
+            $conn->set_charset("utf8");
 
                 if($conn->connect_error){
                 die("Error: ". $conn->connect_error);
